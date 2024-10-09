8c78fa71c38e6a3c1ba84c747a3bee75ad543f30
Wayne Yu <wayne.yu@faire.com>
2024-10-09 19:46:32 -0400

    Add function_log_group_id and function_log_id

--- work-request-exporter/outputs.tf
+++ work-request-exporter/outputs.tf
@@ -1,3 +1,11 @@
 output "function_id" {
   value = oci_functions_function.work_request_exporter_function.id
 }
+
+output "function_log_group_id" {
+  value = module.work_request_exporter_logging.func_loggroupid
+}
+
+output "function_log_id" {
+  value = module.work_request_exporter_logging.func_logid
+}
