3c6dd229e3f6fd24a63570bf5cd5effd56592f7d
Wayne Yu <wayne.yu@faire.com>
2024-10-10 12:12:39 -0400

    move invoke log to platform-configs

--- work-request-exporter/outputs.tf
+++ work-request-exporter/outputs.tf
@@ -1,11 +1,3 @@
 output "function_id" {
   value = oci_functions_function.work_request_exporter_function.id
 }
-
-output "function_log_group_id" {
-  value = module.work_request_exporter_logging.func_loggroupid
-}
-
-output "function_log_id" {
-  value = module.work_request_exporter_logging.func_logid
-}
