628b6b410748fc11d584c00cbbf30b7ebe520dd8
Wayne Yu <wayne.yu@faire.com>
2024-10-10 12:01:27 -0400

    Try hourly

--- work-request-exporter/scheduler.tf
+++ work-request-exporter/scheduler.tf
@@ -1,7 +1,7 @@
 resource "oci_resource_scheduler_schedule" "work_request_exporter_scheduler" {
   action = "START_RESOURCE"
   compartment_id = var.tenancy_ocid
-  recurrence_details = "*/${var.poll_interval_minutes} * * * *"
+  recurrence_details = "* */1 * * *"
   recurrence_type = "CRON"
 
   resources {
