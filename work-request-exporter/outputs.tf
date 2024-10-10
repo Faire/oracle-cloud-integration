e672dec7c9680b27dcaf705f367ed0ab65c02886
Wayne Yu <wayne.yu@faire.com>
2024-10-10 12:25:20 -0400

    Add app id

--- work-request-exporter/outputs.tf
+++ work-request-exporter/outputs.tf
@@ -1,3 +1,7 @@
 output "function_id" {
   value = oci_functions_function.work_request_exporter_function.id
 }
+
+output "function_app_id" {
+  value = oci_functions_application.work_request_exporter_function_app.id
+}
